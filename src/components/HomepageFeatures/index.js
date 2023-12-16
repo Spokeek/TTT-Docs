b13a4a1725a257c979696e740087ed81a155d5c2
Reina_Sakiria <sasiki.64892@gmail.com>
2023-12-16 16:46:52 +0900

    Update: HomePage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,26 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
+    //TODO : このあたりもうちょっと違う画像とかにしてもよいかも...?
+    title: 'AtlasTexture',
+    img: require('@site/static/img/TutorialCard/AtlasTexture-TutorialHeader.png').default,
+    description: (
+      <>
+        非破壊でテクスチャアトラス化！
+      </>
+    ),
+  },
+
+  {
     title: 'SimpleDecal',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    img: require('@site/static/img/TutorialCard/SimpleDecal-TutorialHeader.png').default,
     description: (
       <>
         直感的な色改変！
       </>
     ),
   },
-  {
-    title: 'AtlasTexture',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        非破壊でVRAM削減！
-      </>
-    ),
-  },
+
   // {
   //   title: 'PSD Inproter',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   img: require('@site/static/img/undraw_docusaurus_react.svg').default,
   //   description: (
   //     <>
   //       レイヤー差分をUnityで！
@@ -32,11 +35,11 @@ const FeatureList = [
   // },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ img, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureImg} role="img" />
+        <img className={styles.featureImg} src={img} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
